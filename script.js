4d0623ed67a797de62b7c7fae5237f2f65645d97
Paul Kim <pykim0591@gmail.com>
2018-05-19 10:20:15 -0600

    JS CSS Edits

---
+++ script.js
@@ -0,0 +1,20 @@
+var titleInput = document.querySelector('.title-box');
+var bodyInput = document.querySelector('.body-box');
+var saveButton = document.querySelector('.save-button');
+var searchInput = document.querySelector('.search-input-box');
+
+saveButton.addEventListener('click', userInput;
+searchInput = addEventListener('input', searchFilter;
+
+function userInput() {
+
+}
+
+function searchFilter() {
+
+}
+
+function newIdeaBox() {
+  var createBox = document.createElement('article');
+  var addTitle =  
+}
