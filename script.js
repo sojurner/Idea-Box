d5ec3ed470bee149b9f59343d84c2e3c2d25db55
sojurner <pykim0591@gmail.com>
2018-05-20 21:18:01 -0600

    RevampedJS

--- script.js
+++ script.js
@@ -1,54 +1,78 @@
-var titleInput = document.querySelector('#title-input-box');
-var bodyInput = document.querySelector('#body-input-box');
-var saveButton = document.querySelector('.save-button');
-var searchInput = document.querySelector('#search-input-box');
-var informationSection = document.querySelector('.information-section');
-var titleBodyForm = document.querySelector('.title-body-form');
-var arrowUp = document.querySelector('.up-arrow-button');
-var arrowDown = document.querySelector('.down-arrow-button');
-var qualityRating = document.querySelector('.quality-rating');
+var titleInput = $('#title-input-box');
+var bodyInput = $('#body-input-box');
+var saveButton = $('.save-button');
+var searchInput = $('#search-input-box');
+var infoSection = $('.information-section');
+var titleBodyForm = $('.title-body-form');
+var arrowUp = $('.up-arrow-button');
+var arrowDown = $('.down-arrow-button');
+var qualityRating = $('.quality-rating');
+var IdeaArray = [];
 
-
-saveButton.addEventListener('click', newIdeaBox);
-titleBodyForm.addEventListener('submit', newIdeaBox);
-arrowUp.addEventListener('click', upRateIdeaBox);
+saveButton.on('click', generateNewIdea);
+arrowUp.on('click', ideaEvaluation)
+titleBodyForm.on('submit', newIdeaBox);
+arrowDown.on('click', downRateIdeaBox);
+// infoSection.on('keyup', 'h2, p', function)
 // searchInput.addEventListener('input', searchFilter;)
 
-function newIdeaBox(event) {
-  event.preventDefault()
-  var createBox = document.createElement('article');
-    createBox.setAttribute('class', 'idea-box');
-    createBox.innerHTML =
-    `<h2>${titleInput.value}<span><button class="delete-button"></button></span></h2>
-    <p>${bodyInput.value}</p>
-    <button class="up-arrow-button">up</button>
-    <button class="down-arrow-button">down</button>
-    <p class="quality-title">quality: <p class="quality-rating">swill</p></p>`
-    console.log ('hello');
-  informationSection.prepend(createBox);
+function Idea(title, body, quality, id) {
+  this.title = title;
+  this.body = body;
+  this.quality = quality || 'swill';
+  this.id = id;
 }
 
-function upRateIdeaBox(event) {
-  if (event.qualityRating.value = qualityRating.value) {
-    qualityRating.innerText = 'plausible'
-  }
-  else if (qualityRating.value = 'plausible') {
-    qualityRating.innerText = 'genius'
-  }
-  else if (qualityRating.value = 'genius') {
-    qualityRating.innerText = 'genius';
-  }
+function generateNewIdea(event) {
+  event.preventDefault();
+  id = Date.now()  
+  newCard = new Idea(titleInput.val(), bodyInput.val());
+  prependCard(newCard);
+  ideaArray.push(newCard);
+  // localStorageUpdate();
 }
 
-function downRateIdeaBox() {
-  if (qualityRating.value = 'genius') {
-    qualityRating.innerText = 'plausible'
-  }
-  if (qualityRating.value = 'plausible') {
-    qualityRating.innerText = 'swill'
-  }
-  if (qualityRating.value = 'swill') {
-    qualityRating.innerText = 'swill';
-  }
+//how to link this function to constructor function  
+function prependCard() {
+   var ideaCard =
+  `<article class="idea-box-card" id=${newCard.id});
+    <h2 contenteditable="true">${newCard.title}<span><button class="delete-button"></button></span></h2>
+    <p contenteditable="true">${newCard.body}</p>
+    <button id="upvote" class="up-arrow-button"></button>
+    <button id="downvote" class="down-arrow-button"></button>
+    <p class="quality-title">quality: <p class="quality-rating">${newCard.rating}</p></p>
+    <hr>
+    </article>`;
+    infoSection.prepend(ideaCard);
 }
+
+// DO we Need .text?
+// function ideaEvaluation() {
+// var arrowLink = $(this).attr('class');
+// var parentLink = 
+// ideaRating.indexOf()
+//   captureDown = 
+// if (quality === 'swill') {
+//     quality.text('plausible')
+//   } else if (quality === 'plausible' || quality === 'genius') {
+//     quality.text('genius');
+// }
+
+// idea-box-card.prototype.downVote = function() {
+//   var quality = this.quality; 
+//   if (quality === 'genius') {  
+//     this.quality.text = 'plausible';
+//   } else if (quality === 'plausible' || quality === 'swill') {
+//     this.quality = 'swill';
+//   }
+// }
+// function ideaEvaluation () {
+//   var cardID = $(this).closest('.idea-box-card').attr('id');
+//   var qualityEdit = cardID.find('.quality-rating');
+//   if(.cardID === 'upVote') {
+// }
+
+// storage
+
+
 
