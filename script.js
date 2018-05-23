7d11456aa262418fb95ea8fac7560dbceef506c0
Kevin Ryan <kevinpatryan@gmail.com>
2018-05-23 08:42:25 -0600

    Added CSS

--- script.js
+++ script.js
@@ -1,54 +1,111 @@
-var titleInput = document.querySelector('#title-input-box');
-var bodyInput = document.querySelector('#body-input-box');
-var saveButton = document.querySelector('.save-button');
-var searchInput = document.querySelector('#search-input-box');
-var informationSection = document.querySelector('.information-section');
-var titleBodyForm = document.querySelector('.title-body-form');
-var arrowUp = document.querySelector('.up-arrow-button');
-var arrowDown = document.querySelector('.down-arrow-button');
-var qualityRating = document.querySelector('.quality-rating');
+var titleInput = $('#title-input-box');
+var bodyInput = $('#body-input-box');
+var saveButton = $('.save-button');
+var searchInput = $('#search-input-box');
+var infoSection = $('.information-section');
+var titleBodyForm = $('.title-body-form');
+var qualityRating = $('.quality-rating');
+var ideaBoxCard = $('.idea-box-card');
+
+$(window).on('load', retrievalFromStorage);
 
 
-saveButton.addEventListener('click', newIdeaBox);
-titleBodyForm.addEventListener('submit', newIdeaBox);
-arrowUp.addEventListener('click', upRateIdeaBox);
-// searchInput.addEventListener('input', searchFilter;)
-
-function newIdeaBox(event) {
-  event.preventDefault()
-  var createBox = document.createElement('article');
-    createBox.setAttribute('class', 'idea-box');
-    createBox.innerHTML =
-    `<h2>${titleInput.value}<span><button class="delete-button"></button></span></h2>
-    <p>${bodyInput.value}</p>
-    <button class="up-arrow-button">up</button>
-    <button class="down-arrow-button">down</button>
-    <p class="quality-title">quality: <p class="quality-rating">swill</p></p>`
-    console.log ('hello');
-  informationSection.prepend(createBox);
+function Idea(title, body, id, quality) {
+  this.title = title;
+  this.body = body;
+  this.id = id;
+  this.quality = quality || 'swill';
+}
+//purpose for an array to 
+saveButton.on('click', generateNewIdea);
+function generateNewIdea(event) {
+  event.preventDefault();
+  var id = Date.now();  
+  var newCard = new Idea(titleInput.val(), bodyInput.val(), id);
+  localStorage.setItem(id, JSON.stringify(newCard));
+  prependCard(newCard);
 }
 
-function upRateIdeaBox(event) {
-  if (event.qualityRating.value = qualityRating.value) {
-    qualityRating.innerText = 'plausible'
-  }
-  else if (qualityRating.value = 'plausible') {
-    qualityRating.innerText = 'genius'
-  }
-  else if (qualityRating.value = 'genius') {
-    qualityRating.innerText = 'genius';
-  }
+//how to link this function to constructor function  
+function prependCard(object) {
+   var ideaCard =
+  `<article class="idea-box-card" id="${object.id}">
+    <header class ="card-header">
+      <h2 contenteditable="true">${object.title}
+        <span>
+          <button class="delete-button"></button>
+        </span>
+      </h2>
+    </header>
+    <p contenteditable="true">${object.body}</p>
+    <button id="upvote" class="up-arrow-button"></button>
+    <button id="downvote" class="down-arrow-button" aria 'label'></button>
+    <p class="quality-title">quality: <p class="quality-rating">${object.quality}</p></p>
+    <hr>
+    </article>`;
+    searchInput.after(ideaCard);
 }
 
-function downRateIdeaBox() {
-  if (qualityRating.value = 'genius') {
-    qualityRating.innerText = 'plausible'
+infoSection.on('click', '.up-arrow-button', function() {
+  var upToRating = $(this).closest('.idea-box-card').find('.quality-rating');
+  var getObject  = localStorage.getItem(upToRating);
+  var parseGetObject = JSON.parse(getObject);
+  if (upToRating.text() === 'swill') {
+    upToRating.text('plausible');
+  } else if (upToRating.text() === 'plausible') {
+    upToRating.text('genius');
   }
-  else if (qualityRating.value = 'plausible') {
-    qualityRating.innerText = 'swill'
+  updateRatingToStorage();
+})
+
+infoSection.on('click', '.down-arrow-button', function() {
+  var downToRating = $(this).closest('.idea-box-card').find('.quality-rating'); 
+  var getObject = localStorage.getItem(downToRating);
+  var parseGetObject = JSON.parse(getObject);
+  if (downToRating.text() === 'genius') {  
+    downToRating.text('plausible');
+  } else if (downToRating.text() === 'plausible') {
+    downToRating.text('swill');
   }
-  else if (qualityRating.value = 'swill') {
-    qualityRating.innerText = 'swill';
+})
+
+infoSection.on('click', '.delete-button', function() {
+  console.log($(this).parents('.idea-box-card').attr('id'));
+  if ($(this).hasClass('delete-button')) {
+    $(this).parents('.idea-box-card').remove();
+    localStorage.removeItem($(this.parents('.idea-box=card').attr('id')));
   }
+})
+ // if ($(this).hasClass() === 'delete-button') 
+//----------------------------
+function sendInfoToStorage(Idea, id, title, ) {
+  var stringifyData = json.stringify(newCard)
+  localStorage.setItem(object.title, titleInput.val())
 }
+
+function retrievalFromStorage() {
+  // var newArray = [];
+  for(var i = 0; i < localStorage.length; i++) {
+    var retrieveStorageData = localStorage.getItem(localStorage.key(i))
+    var parseLocalData = JSON.parse(retrieveStorageData);
+    prependCard(parseLocalData);
+  };
+}
+
+searchInput.on("keyup", function(event) {
+  event.preventDefault();
+  var search = $(this).val();
+  // $.extend($.expr[":"], {
+  //   "containsIN": function(elem, i, match, array) {
+  //     return (elem.textContent || elem.innerText || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+  //   }
+  // });
+  $('h2:containsIN(' + search + ')').closest('.idea-box-card').show();
+  $('h2:not(:containsIN(' + search + '))').closest('.idea-box-card').hide();
+  $('p:containsIN(' + search + ')').closest('.idea-box-card').show();
+});
+
+
+
+
 
